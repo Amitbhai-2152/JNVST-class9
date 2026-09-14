1060017e2d1adecde2a24739d4ef9ce96c366a5e
Amit Raj <navodaya2152@gmail.com>
2026-09-14 11:15:10 +0530

    Wire verified 110-question Math bank

--- src/data/questions/math.ts
+++ src/data/questions/math.ts
@@ -1,2 +1,12 @@
-import { generatedQuestions } from './generated';
-export const mathQuestions = generatedQuestions.filter(q => q.subjectId === 'sub_math');
+import type { Question } from '../../types';
+import { mathQuestionsPart1 } from './mathPart1';
+import { mathQuestionsPart2 } from './mathPart2';
+import { mathQuestionsPart3 } from './mathPart3';
+import { mathQuestionsPart4 } from './mathPart4';
+
+export const mathQuestions: Question[] = [
+  ...mathQuestionsPart1,
+  ...mathQuestionsPart2,
+  ...mathQuestionsPart3,
+  ...mathQuestionsPart4,
+];
