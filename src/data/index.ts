749fca38c25874781b4c4fe1ccad064f7fbd7baa
Amit Raj <navodaya2152@gmail.com>
2026-09-21 21:39:42 +0530

    feat: export JNVST exam question selectors

--- src/data/index.ts
+++ src/data/index.ts
@@ -1,4 +1,4 @@
-import { allQuestions, getQuestion, getQuestionsByTopic, getQuestionsBySubject } from './questions';
+import { allQuestions, jnvstExamQuestions, getQuestion, getQuestionsByTopic, getQuestionsBySubject, getJnvstQuestionsBySubject, getJnvstQuestionsByTopic } from './questions';
 import { allLessons, getLesson, getLessonsByTopic } from './lessons';
 import { subjects, chapters, topics } from './curriculum';
 import { jnvstClass9Syllabus } from './syllabus/jnvst-class9';
@@ -6,7 +6,7 @@ import { jnvstClass9Audit, jnvstClass9AuditSummary, jnvstClass9ExtraCurrentTopic
 import { jnvstClass9ContentAudit, jnvstTopicContentAudit, jnvstClass9ContentAuditSummary, jnvstClass9ContentAuditBySubject } from './syllabus/jnvst-class9-content-audit';
 import { jnvstQuestionAudit, jnvstQuestionAuditStatus } from './questions/questionAudit';
 
-export { subjects, chapters, topics, allQuestions, allLessons, getQuestion, getLesson, getLessonsByTopic, getQuestionsByTopic, getQuestionsBySubject, jnvstClass9Syllabus, jnvstClass9Audit, jnvstClass9AuditSummary, jnvstClass9ExtraCurrentTopics, jnvstClass9ContentAudit, jnvstTopicContentAudit, jnvstClass9ContentAuditSummary, jnvstClass9ContentAuditBySubject, jnvstQuestionAudit, jnvstQuestionAuditStatus };
+export { subjects, chapters, topics, allQuestions, jnvstExamQuestions, allLessons, getQuestion, getLesson, getLessonsByTopic, getQuestionsByTopic, getQuestionsBySubject, getJnvstQuestionsBySubject, getJnvstQuestionsByTopic, jnvstClass9Syllabus, jnvstClass9Audit, jnvstClass9AuditSummary, jnvstClass9ExtraCurrentTopics, jnvstClass9ContentAudit, jnvstTopicContentAudit, jnvstClass9ContentAuditSummary, jnvstClass9ContentAuditBySubject, jnvstQuestionAudit, jnvstQuestionAuditStatus };
 
 export const getSubject = (id: string) => subjects.find((subject) => subject.id === id);
 export const getChapters = (subjectId: string) => chapters.filter((chapter) => chapter.subjectId === subjectId).sort((a, b) => a.order - b.order);
