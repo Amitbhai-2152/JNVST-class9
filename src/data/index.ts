cafad3b0df8b4ef5ae906090df085e339b14873d
Amit Raj <navodaya2152@gmail.com>
2026-09-21 21:54:51 +0530

    feat: export Phase 5 content status

--- src/data/index.ts
+++ src/data/index.ts
@@ -6,8 +6,9 @@ import { jnvstClass9Audit, jnvstClass9AuditSummary, jnvstClass9ExtraCurrentTopic
 import { jnvstClass9ContentAudit, jnvstTopicContentAudit, jnvstClass9ContentAuditSummary, jnvstClass9ContentAuditBySubject } from './syllabus/jnvst-class9-content-audit';
 import { jnvstQuestionAudit, jnvstQuestionAuditStatus } from './questions/questionAudit';
 import { jnvstClass9Phase4Complete } from '../utils/jnvstIntelligence';
+import { phase5Enhancements, jnvstClass9Phase5Complete } from './lessons/phase5Content';
 
-export { subjects, chapters, topics, allQuestions, jnvstExamQuestions, allLessons, getQuestion, getLesson, getLessonsByTopic, getQuestionsByTopic, getQuestionsBySubject, getJnvstQuestionsBySubject, getJnvstQuestionsByTopic, jnvstClass9Syllabus, jnvstClass9Audit, jnvstClass9AuditSummary, jnvstClass9ExtraCurrentTopics, jnvstClass9ContentAudit, jnvstTopicContentAudit, jnvstClass9ContentAuditSummary, jnvstClass9ContentAuditBySubject, jnvstQuestionAudit, jnvstQuestionAuditStatus, jnvstClass9Phase4Complete };
+export { subjects, chapters, topics, allQuestions, jnvstExamQuestions, allLessons, getQuestion, getLesson, getLessonsByTopic, getQuestionsByTopic, getQuestionsBySubject, getJnvstQuestionsBySubject, getJnvstQuestionsByTopic, jnvstClass9Syllabus, jnvstClass9Audit, jnvstClass9AuditSummary, jnvstClass9ExtraCurrentTopics, jnvstClass9ContentAudit, jnvstTopicContentAudit, jnvstClass9ContentAuditSummary, jnvstClass9ContentAuditBySubject, jnvstQuestionAudit, jnvstQuestionAuditStatus, jnvstClass9Phase4Complete, phase5Enhancements, jnvstClass9Phase5Complete };
 
 export const getSubject = (id: string) => subjects.find((subject) => subject.id === id);
 export const getChapters = (subjectId: string) => chapters.filter((chapter) => chapter.subjectId === subjectId).sort((a, b) => a.order - b.order);
