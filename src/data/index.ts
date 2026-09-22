764081e7e0ed5f83c4a4dda6382865db2d6bb4a8
Amit Raj <navodaya2152@gmail.com>
2026-09-22 06:55:07 +0530

    feat: export math quality audit

--- src/data/index.ts
+++ src/data/index.ts
@@ -7,8 +7,9 @@ import { jnvstClass9ContentAudit, jnvstTopicContentAudit, jnvstClass9ContentAudi
 import { jnvstQuestionAudit, jnvstQuestionAuditStatus } from './questions/questionAudit';
 import { jnvstClass9Phase4Complete } from '../utils/jnvstIntelligence';
 import { phase5Enhancements, jnvstClass9Phase5Complete } from './lessons/phase5Content';
+import { jnvstMathQualityAudit, jnvstMathQualityStatus } from './questions/mathQualityAudit';
 
-export { subjects, chapters, topics, allQuestions, jnvstExamQuestions, allLessons, getQuestion, getLesson, getLessonsByTopic, getQuestionsByTopic, getQuestionsBySubject, getJnvstQuestionsBySubject, getJnvstQuestionsByTopic, jnvstClass9Syllabus, jnvstClass9Audit, jnvstClass9AuditSummary, jnvstClass9ExtraCurrentTopics, jnvstClass9ContentAudit, jnvstTopicContentAudit, jnvstClass9ContentAuditSummary, jnvstClass9ContentAuditBySubject, jnvstQuestionAudit, jnvstQuestionAuditStatus, jnvstClass9Phase4Complete, phase5Enhancements, jnvstClass9Phase5Complete };
+export { subjects, chapters, topics, allQuestions, jnvstExamQuestions, allLessons, getQuestion, getLesson, getLessonsByTopic, getQuestionsByTopic, getQuestionsBySubject, getJnvstQuestionsBySubject, getJnvstQuestionsByTopic, jnvstClass9Syllabus, jnvstClass9Audit, jnvstClass9AuditSummary, jnvstClass9ExtraCurrentTopics, jnvstClass9ContentAudit, jnvstTopicContentAudit, jnvstClass9ContentAuditSummary, jnvstClass9ContentAuditBySubject, jnvstQuestionAudit, jnvstQuestionAuditStatus, jnvstClass9Phase4Complete, phase5Enhancements, jnvstClass9Phase5Complete, jnvstMathQualityAudit, jnvstMathQualityStatus };
 
 export const getSubject = (id: string) => subjects.find((subject) => subject.id === id);
 export const getChapters = (subjectId: string) => chapters.filter((chapter) => chapter.subjectId === subjectId).sort((a, b) => a.order - b.order);
