ed0d85d9706816b1db4fe24c78c30327f4482b8e
Amit Raj <navodaya2152@gmail.com>
2026-09-22 07:07:06 +0530

    fix: export existing content audit members

--- src/data/index.ts
+++ src/data/index.ts
@@ -3,13 +3,13 @@ import { allLessons, getLesson, getLessonsByTopic } from './lessons';
 import { subjects, chapters, topics } from './curriculum';
 import { jnvstClass9Syllabus } from './syllabus/jnvst-class9';
 import { jnvstClass9Audit, jnvstClass9AuditSummary, jnvstClass9ExtraCurrentTopics } from './syllabus/jnvst-class9-audit';
-import { jnvstClass9ContentAudit, jnvstTopicContentAudit, jnvstClass9ContentAuditSummary, jnvstClass9ContentAuditBySubject } from './syllabus/jnvst-class9-content-audit';
+import { jnvstTopicContentAudit, jnvstClass9ContentAuditSummary, jnvstClass9ContentAuditBySubject } from './syllabus/jnvst-class9-content-audit';
 import { jnvstQuestionAudit, jnvstQuestionAuditStatus } from './questions/questionAudit';
 import { jnvstClass9Phase4Complete } from '../utils/jnvstIntelligence';
 import { phase5Enhancements, jnvstClass9Phase5Complete } from './lessons/phase5Content';
 import { jnvstMathQualityAudit, jnvstMathQualityStatus } from './questions/mathQualityAudit';
 
-export { subjects, chapters, topics, allQuestions, jnvstExamQuestions, allLessons, getQuestion, getLesson, getLessonsByTopic, getQuestionsByTopic, getQuestionsBySubject, getJnvstQuestionsBySubject, getJnvstQuestionsByTopic, jnvstClass9Syllabus, jnvstClass9Audit, jnvstClass9AuditSummary, jnvstClass9ExtraCurrentTopics, jnvstClass9ContentAudit, jnvstTopicContentAudit, jnvstClass9ContentAuditSummary, jnvstClass9ContentAuditBySubject, jnvstQuestionAudit, jnvstQuestionAuditStatus, jnvstClass9Phase4Complete, phase5Enhancements, jnvstClass9Phase5Complete, jnvstMathQualityAudit, jnvstMathQualityStatus };
+export { subjects, chapters, topics, allQuestions, jnvstExamQuestions, allLessons, getQuestion, getLesson, getLessonsByTopic, getQuestionsByTopic, getQuestionsBySubject, getJnvstQuestionsBySubject, getJnvstQuestionsByTopic, jnvstClass9Syllabus, jnvstClass9Audit, jnvstClass9AuditSummary, jnvstClass9ExtraCurrentTopics, jnvstTopicContentAudit, jnvstClass9ContentAuditSummary, jnvstClass9ContentAuditBySubject, jnvstQuestionAudit, jnvstQuestionAuditStatus, jnvstClass9Phase4Complete, phase5Enhancements, jnvstClass9Phase5Complete, jnvstMathQualityAudit, jnvstMathQualityStatus };
 
 export const getSubject = (id: string) => subjects.find((subject) => subject.id === id);
 export const getChapters = (subjectId: string) => chapters.filter((chapter) => chapter.subjectId === subjectId).sort((a, b) => a.order - b.order);
